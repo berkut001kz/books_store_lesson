c55ee5d9c3f977a0f67ee4c5f5b5d0cda5d9753c
Еламан Бақытжанұлы <programmer.site.kz@gmail.com>
2021-05-11 05:45:21 +0600

    Book class-тын жеке ақпаратын анықтадық

---
+++ domain/Books_Store_Lesson/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Books_Store_Lesson
+{
+    public class Book
+    {
+        #region Getters
+
+        public int Id { get; }
+        public string Title { get; }
+
+        #endregion Getters
+
+        #region Constructor
+
+        public Book(int id, string title)
+        {
+            this.Id = id;
+            this.Title = title;
+        }
+
+        #endregion Constructor
+
+    }
+}
