b93819976abb0256a56af1a5b7cccbb8e24f864e
Еламан Бақытжанұлы <programmer.site.kz@gmail.com>
2021-05-11 08:52:40 +0600

    Веб-қосымша жасаңыз

---
+++ presentation/Books_Store_Lesson.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Books_Store_Lesson.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
