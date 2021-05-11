488197220ab53ef3cbcfeaae3090a0b98cef6011
Еламан Бақытжанұлы <programmer.site.kz@gmail.com>
2021-05-11 06:08:20 +0600

    Interface құрдық аты IBookRepository

---
+++ domain/Books_Store_Lesson/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books_Store_Lesson
+{
+    // Interface - неге құрдық
+    // Біз сақтау орны әдісін жасамадық сондықтан
+    // Белгісіз болған жағдайда Interface қажет
+    public interface IBookRepository
+    {
+        Book[] GetAllByTitle(string titlePart);
+    }
+}
