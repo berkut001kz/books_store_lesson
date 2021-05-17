07c4a4dd3f2056ec549146caf9d61048bdf8f517
Еламан Бақытжанұлы <programmer.site.kz@gmail.com>
2021-05-17 19:50:01 +0600

    Кітаптың сипаттамасын қосыңыз

--- domain/Books_Store_Lesson/IBookRepository.cs
+++ domain/Books_Store_Lesson/IBookRepository.cs
@@ -10,6 +10,7 @@ namespace Books_Store_Lesson
     public interface IBookRepository
     {
         Book[] GetAllByIsbn(string isbn);
-        Book[] GetAllByTitleOrAuthor(string titleOrAuthor); 
+        Book[] GetAllByTitleOrAuthor(string titleOrAuthor);
+        Book GetById(int id);
     }
 }
